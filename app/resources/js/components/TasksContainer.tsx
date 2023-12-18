ead639db1156ddc221402fb28c776b2cb059563c
Lagacy_code <miraglioenzo93@gmail.com>
2023-12-18 17:48:58 +0100

    style : hover effects, tasks positionning, color corrresponding to status

--- app/resources/js/components/TasksContainer.tsx
+++ app/resources/js/components/TasksContainer.tsx
@@ -3,10 +3,93 @@ import React, { ReactNode, useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const WrapperTasks = styled.section`
+  position: relative;
+  padding-inline: 10%;
   display: grid;
   grid-template-rows: auto auto 1fr;
+  gap: 2em;
   place-items: center;
   color: white;
+  & > ol#tasksCont {
+    display: flex;
+    gap: 2em;
+    flex-wrap: wrap;
+    justify-self: start;
+    align-self: start;
+    width: 100%;
+    & > li {
+      position: relative;
+      display: grid;
+      grid-template-rows: auto auto 75px;
+      padding-block: 1em;
+      gap: 1em;
+      text-align: center;
+      width: clamp(250px, 25%, 350px);
+      border-radius: 10px;
+      overflow: hidden;
+
+      &.todo {
+        background-color: rgba(27, 25, 77, 0.75);
+        & > .statusOverlay {
+          background-color: hsl(242, 50%, 20%);
+        }
+      }
+      &.doing {
+        background-color: rgba(51, 136, 239, 0.75);
+        & > .statusOverlay {
+          background-color: #3389ef;
+        }
+      }
+      &.done {
+        background-color: rgba(111, 218, 92, 0.75);
+        & > .statusOverlay {
+          background-color: #6eda5c;
+        }
+      }
+
+      & > p {
+        font-size: 1.25em;
+      }
+      & > .inputStatus {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 1em;
+        & > label {
+          display: flex;
+          font-size: 0.9em;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          gap: 0.25em;
+          margin-top: 1em;
+
+          & input:hover {
+            cursor: pointer;
+          }
+        }
+      }
+
+      &:hover {
+        & > .statusOverlay {
+          transform: translateY(100%);
+        }
+      }
+
+      & > .statusOverlay {
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: all 0.2s ease-out;
+        bottom: 0;
+        width: 100%;
+        transform: translateY(0%);
+        height: 75px;
+        border-radius: 0 0 8px 8px;
+      }
+    }
+  }
 `
 
 const TasksContainer = ({ tasks, children }: { tasks: TasksType; children: ReactNode }) => {
@@ -71,6 +154,7 @@ const TasksContainer = ({ tasks, children }: { tasks: TasksType; children: React
             value={progression.toPrecision(2)}
             min={0}
             max={100}
+            step={1}
             disabled
           />
         </label>
@@ -79,7 +163,7 @@ const TasksContainer = ({ tasks, children }: { tasks: TasksType; children: React
         {dinamicTasks?.map((task, index) => {
           const status = task.status === 0 ? 'todo' : task.status === 1 ? 'doing' : 'done'
           return (
-            <li key={task.id}>
+            <li key={task.id} className={status}>
               <h2>{task.name}</h2>
               <p>{task.description}</p>
               <div className="inputStatus">
